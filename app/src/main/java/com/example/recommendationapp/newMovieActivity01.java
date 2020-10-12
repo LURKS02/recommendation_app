3e435a4aa00920d805ac5f7090f4402eaa7e4d0e
LURKS02 <thecrooks2@gmail.com>
2020-10-13 00:36:48 +0900

    intent test

---
+++ app/src/main/java/com/example/recommendationapp/newMovieActivity01.java
@@ -0,0 +1,14 @@
+package com.example.recommendationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newMovieActivity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_movie01);
+    }
+}
